74bf70504670aeb7c812d4662509fbca8ce0889e
Mirza Buğra Özçelik <120049049+ozcelikmirzabugra@users.noreply.github.com>
2024-01-31 01:13:48 +0300

    Delete Part 3 directory

--- Part 3/quiz-app/quiz.js
+++
@@ -1,9 +0,0 @@
-function Quiz(questions) {
-  this.questions = questions;
-  this.question_index = 0;
-  this.true_answer_counts = 0;
-}
-
-Quiz.prototype.call_question = function () {
-  return this.questions[this.question_index];
-};
